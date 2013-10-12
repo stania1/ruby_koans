04472bba8d8c4a78816f58906b2e5e5072127e06
Sofia Tania <stania@staniamac.local>
2013-10-12 20:45:29 +0800

    Completed triangle project part 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -21,8 +21,38 @@ def is_isosceles(a, b, c)
   (a == b and b != c) or (b == c and a != b) or (a == c and b != c)
 end
 
+def positive(x)
+  if x <= 0
+    raise TriangleError, "Side length cannot be zero"
+  end
+end
+
+def validate_positive(a, b, c)
+  positive(a)
+  positive(b)
+  positive(c)
+end
+
+def validate_shorter_sides_add_up_to_more_than_longest_side(a, b, c) # TODO find a shorter name
+  sides = [a, b, c]
+  max = sides.max
+  if max == a and b + c <= max
+    raise TriangleError, "Invalid triangle"
+  elsif max == b and a + c <= max
+    raise TriangleError, "Invalid triangle" # duplication!
+  elsif max == c and a + b <= max
+    raise TriangleError, "Invalid triangle"
+  end
+end
+
+def validate(a, b, c)
+  validate_positive(a, b, c)
+  validate_shorter_sides_add_up_to_more_than_longest_side(a, b, c)  
+end
+
 def triangle(a, b, c)
   # WRITE THIS CODE
+  validate(a, b, c)
   if is_equilateral(a, b, c)
     :equilateral
   elsif is_isosceles(a, b, c)
